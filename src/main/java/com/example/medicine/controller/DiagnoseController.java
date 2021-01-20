c381fb81f876cec9f4776e27a122e5e78f9aa771
Shabdan <shabdan.abjaparov.official@gmail.com>
2021-01-20 12:45:11 +0600

    fixing issues with DiagnoseController.java

--- src/main/java/com/example/medicine/controller/DiagnoseController.java
+++ src/main/java/com/example/medicine/controller/DiagnoseController.java
@@ -11,7 +11,7 @@ import java.security.Principal;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping(name = "/api/diagnose")
+@RequestMapping(name = "/api/diagnos")
 public class DiagnoseController{
     @Autowired
     DiagnoseService diagnoseService;
